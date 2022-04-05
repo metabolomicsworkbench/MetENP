bec6c09a065557d3363e19a30b655359174775af
susrinivasan <91990424+susrinivasan@users.noreply.github.com>
2022-04-05 11:03:41 -0700

    Update pathinfo.R
    keggGet call for certain reactions does not return ENZYME column. When a column name is extracted into a list that does not exist, it puts NA. Renamed all columns of df to be consistent.

--- R/pathinfo.R
+++ R/pathinfo.R
@@ -22,10 +22,18 @@ pathinfo = function(res, col_name)
   extract_info <- lapply(unlist_info, '[', c("ENTRY","NAME","PATHWAY","RCLASS","ORTHOLOGY","DEFINITION","EQUATION","ENZYME"))
   dd=do.call(rbind, extract_info)
   df = as.data.frame(dd)
+  ## BUG FIX; Added by Sumana Srinivasan 0n 04042022
+  ### named all columns to be consistent
+  names(df)[1] = 'ENTRY'
   names(df)[2] = 'NAME'
   names(df)[3] = 'PATHWAY'
+  names(df)[4] = 'RCLASS'            
   names(df)[5] = 'ORTHOLOGY'
   names(df)[6] = 'EQUATION_more'
+  names(df)[7] = 'EQUATION'
+  names(df)[8] = 'ENZYME' 
+  ## BUG FIX; Added by Sumana Srinivasan 0n 04042022, for rno, the ORTHOLOGY ENZYME column, keggGET returns NA and hence have to be named
+  
   ###pathway
   if (col_name == "PATHWAY"){
   path_info = df[,c("ENTRY","NAME","PATHWAY")]
@@ -62,3 +70,11 @@ pathinfo = function(res, col_name)
  
   return(path_info3)
 }
+               
+##### Test code, Sumana Srinivasan, 05052022 #############
+# rxn_ids <- c("R00021", "R00086") ## give rxn ids that give NA
+# info = llply(rxn_ids, function(x)keggGet(x))
+# unlist_info <- unlist(info, recursive = F)
+# extract_info <- lapply(unlist_info, '[', c("ENTRY","NAME","PATHWAY","RCLASS","ORTHOLOGY","DEFINITION","EQUATION","ENZYME"))
+# dd=do.call(rbind, extract_info)
+# df = as.data.frame(dd)
